606f3e2f8577984cb441388922cce9006341742c
Liam Gessman <Liamngessman+frc@gmail.com>
2025-07-10 10:24:57 -0400

    finished led logic shit

--- Software/src/indicators/led.hpp
+++ Software/src/indicators/led.hpp
@@ -2,75 +2,80 @@
 #define LED_HPP
 
 #include <Arduino.h>
-#include <../include/constants.hpp>
+#include "../include/constants.hpp"
 
-Pins pin;
-class R{
+class LED {
+private:
+    Pins pin;
+
+    struct LEDState {
+        bool active = false;
+        unsigned long startT = 0;
+    };
+
+    LEDState redState, greenState, blueState;
+
+    void handleFlash(LEDState &state, int delayMs, uint8_t ledPin) {
+        if (!state.active) {
+            state.active = true;
+            state.startT = millis();
+            digitalWrite(ledPin, HIGH);
+        } else if (millis() - state.startT >= (unsigned long)delayMs) {
+            digitalWrite(ledPin, LOW);
+            state.active = false;
+        }
+    }
+
+public:
+    class R {
+        LED* parent;
     public:
-        void on(){
-            digitalWrite(pin.LEDR, HIGH);
-        }
-        void off(){
-            digitalWrite(pin.LEDR, LOW);
-        }
-};
-class G{
+        R(LED* p) : parent(p) {}
+        void on()  { digitalWrite(parent->pin.LEDR, HIGH); }
+        void off() { digitalWrite(parent->pin.LEDR, LOW);  }
+    } r{this};
+
+    class G {
+        LED* parent;
     public:
-        void on(){
-            digitalWrite(pin.LEDG, HIGH);
-        }
-        void off(){
-            digitalWrite(pin.LEDG, LOW);
-        }
-};
-class B{
+        G(LED* p) : parent(p) {}
+        void on()  { digitalWrite(parent->pin.LEDG, HIGH); }
+        void off() { digitalWrite(parent->pin.LEDG, LOW);  }
+    } g{this};
+
+    class B {
+        LED* parent;
     public:
-        void on(){
-            digitalWrite(pin.LEDB, HIGH);
-        }
-        void off(){
-            digitalWrite(pin.LEDB, LOW);
-        }
-};
-class LED{
-    private:
-        int precmd = 0;
-    public:
-        R r; // led.r.on() or led.r.off()
-        G g;
-        B b;
-        
-        void ledSetup(){
-            pinMode(pin.LEDR, OUTPUT);
-            pinMode(pin.LEDG, OUTPUT);
-            pinMode(pin.LEDB, OUTPUT);
-        }
+        B(LED* p) : parent(p) {}
+        void on()  { digitalWrite(parent->pin.LEDB, HIGH); }
+        void off() { digitalWrite(parent->pin.LEDB, LOW);  }
+    } b{this};
 
+    void ledSetup(Pins p) {
+        pin = p;
+        pinMode(pin.LEDR, OUTPUT);
+        pinMode(pin.LEDG, OUTPUT);
+        pinMode(pin.LEDB, OUTPUT);
+    }
 
-        void status(int stat){
-            switch (stat){
-                case 1: 
-                case 2:
-                case 3:
-            }
+    void status(int stat) {
+        // Example statuses:
+        r.off(); g.off(); b.off();
+        switch (stat) {
+            case 1: g.on(); break;
+            case 2: r.on(); break;
+            case 3: b.on(); break;
+            default: break;
         }
-        void flash(int delay, int col){
-            switch (col){
-                case 1: 
-                    r.on();
-                       
-            }
+    }
+
+    void flash(int delayMs, int color) {
+        switch (color) {
+            case 1: handleFlash(redState, delayMs, pin.LEDR); break;
+            case 2: handleFlash(greenState, delayMs, pin.LEDG); break;
+            case 3: handleFlash(blueState, delayMs, pin.LEDB); break;
         }
-        void ledPeriodic(int cmd) {
-            if(precmd != cmd){
-
-            }
-            else{
-                return;
-            }
-        }
-
-
+    }
 };
 
 #endif

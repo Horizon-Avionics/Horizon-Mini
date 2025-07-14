166029079edb614d5a2024859e12d329e3f75901
Liam Gessman <Liamngessman+frc@gmail.com>
2025-07-14 11:16:21 -0400

    software stuff idk

--- Software/include/constants.hpp
+++ Software/include/constants.hpp
@@ -4,15 +4,15 @@
 #include <Arduino.h>
 
 struct Pins {
-    static constexpr int LEDR = 0;
-    static constexpr int LEDG = 0;
-    static constexpr int LEDB = 0;
+    public:
+        static constexpr int LEDR = 0;
+        static constexpr int LEDG = 0;
+        static constexpr int LEDB = 0;
 
-    static constexpr int buz = 0;
-    
-    static constexpr int sdCS = 0;
-    static constexpr int nandCS = 0;
+        static constexpr int buz = 0;
+
+        static constexpr int sdCS = 0;
+        static constexpr int nandCS = 0;
 };
-
 
 #endif
---
+++ Software/src/indicators/buzzer.hpp
@@ -0,0 +1,9 @@
+#ifndef BUZZER_HPP
+#define BUZZER_HPP
+
+#include <Arduino.h>
+#include <Adafruit_SPIFlash.h>
+
+class 
+
+#endif //buzzer_hpp
